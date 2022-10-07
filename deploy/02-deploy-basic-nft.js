5183791a696c7b650b9b4206170caa3e0532ecec
bchaindeveloper <bcdev3@protonmail.com>
2022-10-07 16:04:26 -0400

    BasicNft

--- deploy/02-deploy-basic-nft.js
+++ deploy/02-deploy-basic-nft.js
@@ -5,25 +5,23 @@ const { verify } = require("../utils/verify")
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-    const waitBlockConfirmations = developmentChains.includes(network.name)
-        ? 1
-        : VERIFICATION_BLOCK_CONFIRMATIONS
+
 
     log("----------------------------------------------------")
-    const arguments = []
-    const nftMarketplace = await deploy("NftMarketplace", {
+    const args = []
+    const basicNft = await deploy("BasicNft", {
         from: deployer,
-        args: arguments,
+        args: args,
         log: true,
-        waitConfirmations: waitBlockConfirmations,
+        waitConfirmations: network.config.waitBlockConfirmations || 1,
     })
 
     // Verify the deployment
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         log("Verifying...")
-        await verify(nftMarketplace.address, arguments)
+        await verify(basicNft.address, args)
     }
     log("----------------------------------------------------")
 }
 
-module.exports.tags = ["all", "nftmarketplace"]
+module.exports.tags = ["all", "basicnft"]
